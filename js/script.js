a8ccc6c8f4bbc38fff31103262cfebed9abda2eb
DiCaprioLeonardo <149061169+DiCaprioLeonardo@users.noreply.github.com>
2024-05-20 19:08:05 +0500

    script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ const btn = document.getElementById("navbar-toggler");
 const logo = document.querySelector(".logo");
 const menu = document.querySelector(".menu");
 
+const menuWrapper = document.getElementById('menu__wrapper');
+
 btn.addEventListener('click', () => {
     logo.classList.toggle("show");
-    menu.classList.toggle("show");    
+    menu.classList.toggle("show");
+    /*menuWrapper.classList("transorm");*/
 });
